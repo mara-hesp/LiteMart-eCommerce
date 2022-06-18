6386d18fc3599b29f6891fbbdbecb472fd72bf70
Mara Hernandez Esp <98224912+mara-hesp@users.noreply.github.com>
2022-06-18 13:53:08 -0500

    remove-solved

--- src/components/CartItem/CartItem.js
+++ src/components/CartItem/CartItem.js
@@ -1,12 +1,11 @@
-import { useContext, useState } from 'react'
+import { useContext } from 'react'
 import CartContext from '../../context/CartContext'
 
 const CartItem = ({ name, count, price, id }) => {
-    const [clickRemove, setClickRemove] = useState({})
     const { removeItems } = useContext(CartContext)
 
-    const handleRemove = () => {
-        setClickRemove(removeItems)
+    const handleRemove = (id) => {
+        removeItems(id)
     }
 
     return (
@@ -15,7 +14,7 @@ const CartItem = ({ name, count, price, id }) => {
                 <p className='cartName'>{name}</p>
                 <p className='cartPrice'>${price}</p>
                 <p className='cartQuantity'>{count}</p>
-                <div className='cartRemove'><button className='btnCartItem' onClick={handleRemove}>x</button></div>
+                <div className='cartRemove'><button className='btnCartItem' onClick={() => handleRemove(id)}>x</button></div>
             </li>
         </div>
     )
